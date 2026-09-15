7b48ecbf7fea88f6151f33e2e126d874a15769df
slower232 <ivancanelito@gmail.com>
2026-09-15 11:40:25 -0600

    Update Clase.java
    Primera clase. Encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clase.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Clase {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Clase extends OpMode {
+
+    DcMotor armM;
+
+    @Override
+    public void init() {
+        armM = hardwareMap.dcMotor.get("armM");
+
+        armM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armM.setTargetPosition(0);
+        armM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a){
+            moverBrazo(-300);
+        } else if (gamepad1.b) {
+            moverBrazo(0);
+        }
+
+        telemetry.addData("armPos", armM.getCurrentPosition());
+        telemetry.addData("arnTarget", armM.getTargetPosition());
+        telemetry.addData("armIsBussy", armM.isBusy());
+        telemetry.addData("armMode", armM.getMode());
+
+    }
+
+    void moverBrazo(int position){
+        armM.setTargetPosition(position);
+        armM.setPower(0.5);
+
+    }
 }
